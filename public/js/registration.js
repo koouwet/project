4a4bd43640344b6e14c27b23a7301520015e431b
Catzomm <artpogud@gmail.com>
2025-05-30 00:13:01 +0300

    finish

--- public/js/registration.js
+++
@@ -1,7 +0,0 @@
-form.addEventListener("submit", async (e) => {
-    const res = await fetch("/api/register", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name, password })
-    });
-  });
--- src/config.js
+++
@@ -1,8 +0,0 @@
-import { readFile } from "node:fs/promises";
-import path from "node:path";
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const configPath = path.join(__dirname, '..', 'config', 'config.json');
-export const config = JSON.parse(await readFile(configPath, "utf-8"));
